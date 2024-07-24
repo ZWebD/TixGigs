6cc395ea01882574e8a17281b352be83d85b4174
ZWebD <zoltan.szabo.web@gmail.com>
2024-07-24 19:59:06 +0100

    searching updates and changes

--- actions/getDataOnServer.ts
+++ actions/getDataOnServer.ts
@@ -1,23 +1,19 @@
 "use server";
 
-export type Data =
-  | [
-      {
-        name: string;
-        url: string;
-        promoter: string;
-        images: [
-          {
-            ratio: string;
-            url: string;
-            width: number;
-            height: number;
-            fallback: boolean;
-          }
-        ];
-      }
-    ]
-  | undefined;
+export type EventData = {
+  name: string;
+  url: string;
+  promoter: string;
+  images: {
+    ratio: string;
+    url: string;
+    width: number;
+    height: number;
+    fallback: boolean;
+  }[];
+};
+
+export type Data = EventData[];
 
 const apiKey = process.env.NEXT_PUBLIC_API_KEY;
 const keyword = "concert";
@@ -41,20 +37,45 @@ export const getDataOnServer = async (props: any) => {
   !props
     ? (url = `https://app.ticketmaster.com/discovery/v2/events.json?keyword=${keyword}&startDate=${startDate}&endDate=${endDate}&sortBy=${sortBy}&size=${size}&apikey=${apiKey}&countryCode=${countryCode}`)
     : (url = `https://app.ticketmaster.com/discovery/v2/events.json?keyword=${props}&apikey=${apiKey}&countryCode=${countryCode}`);
-  let data: Data;
+
+  let data: Data = [];
   let isError = false;
   let error = "";
 
   try {
     const res = await fetch(url);
-    const filter: any = res
-      .json()
-      .then((result) => [
-        ...new Map(
-          result._embedded.events.map((e: any) => [e.name.toLowerCase(), e])
-        ).values(),
-      ]);
-    data = await filter;
+    const result = await res.json();
+
+    if (result._embedded && result._embedded.events) {
+      const uniqueEventsMap = new Map();
+
+      result._embedded.events.forEach((event: any) => {
+        if (!uniqueEventsMap.has(event.name.toLowerCase())) {
+          uniqueEventsMap.set(event.name.toLowerCase(), {
+            name: event.name,
+            url: event.url,
+            promoter: event.promoter,
+            images: event.images.map((img: any) => ({
+              ratio: img.ratio,
+              url: img.url,
+              width: img.width,
+              height: img.height,
+              fallback: img.fallback,
+            })),
+          });
+        }
+      });
+
+      data = Array.from(uniqueEventsMap.values());
+    }
+    // const filter: any = res
+    //   .json()
+    //   .then((result) => [
+    //     ...new Map(
+    //       result._embedded.events.map((e: any) => [e.name.toLowerCase(), e])
+    //     ).values(),
+    //   ]);
+    // data = await filter;
   } catch (e) {
     isError = true;
     if (typeof e === "string") error = e;
